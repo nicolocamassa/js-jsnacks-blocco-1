32616d856dc338ccbc4a2073a351c9da6b2af7cf
nicolocamassa <camassanicolo03@gmail.com>
2023-11-24 15:19:29 +0100

    [UPDATE] random number generation added

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -2,4 +2,8 @@
 let arrayLength = parseInt(prompt("Quanti elementi dovrà contenere l'array?"));
 let arrayRandom = [];
 
-
+/* For ripetuto in base all'input dell'utente */
+for(let i = 1; i<=arrayLength; i++){
+    /* Generazione dei numeri casuali */
+    let randomNum = Math.floor(Math.random() * 100) + 1;
+}
