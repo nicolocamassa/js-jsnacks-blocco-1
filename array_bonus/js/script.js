6458dbda18314bbabf2f74dd3855f1a15828c14d
nicolocamassa <camassanicolo03@gmail.com>
2023-11-24 15:51:33 +0100

    [BONUS FINITO] comments added

--- array_bonus/js/script.js
+++ array_bonus/js/script.js
@@ -1,20 +1,29 @@
+/* Inizializzazione dei due array */
 let array2 = [1, 2, 3, 4, 5];
 let array1 = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
+/* Visualizzazione dei due array originali */
+console.log(array1)
+console.log(array2)
+
+/* Limitazione del for in base alla differenza della lunghezza dei due array */
 let loopArray = array1.length - array2.length;
 let loopArray2 = array2.length - array1.length;
 
 if(array1.length < array2.length){
+    /* Se il primo array è più corto aggiunge numeri casuali in base alla differenza */
     for(let i=0; i < loopArray2; i++){
         let randomNum = Math.floor(Math.random() * 100);
         array1.push(randomNum);
     }
 }else{
+    /* Se il primo array è più corto aggiunge numeri casuali in base alla differenza */
     for(let i=0; i < loopArray; i++){
         let randomNum = Math.floor(Math.random() * 100);
         array2.push(randomNum);
     }
 }
 
+/* Visualizzazione dei due array di lunghezza uguale */
 console.log(array1);
 console.log(array2);
