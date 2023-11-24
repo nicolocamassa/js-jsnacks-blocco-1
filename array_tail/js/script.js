040012b805f94b291964c25c638495921ac9b462
nicolocamassa <camassanicolo03@gmail.com>
2023-11-24 15:33:06 +0100

    [ARRAY-TAIL FINITO] output last 5 number

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -6,4 +6,18 @@ let arrayRandom = [];
 for(let i = 1; i<=arrayLength; i++){
     /* Generazione dei numeri casuali */
     let randomNum = Math.floor(Math.random() * 100) + 1;
+    arrayRandom.push(randomNum);
 }
+
+/* Inizio del conteggio del for dalla fine */
+let startArray = arrayRandom.length - 1;
+
+/* Limite del conteggio per 5 numeri */
+let limitFor = arrayRandom.length - 6;
+
+/* Stampa gli ultimi 5 numeri decrementando */
+for(let i = startArray; i>limitFor; i--){
+    console.log(arrayRandom[i]);
+}
+
+
