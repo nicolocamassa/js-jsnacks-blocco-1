25acb750ba48d4ffd6a153c32df50be3353df00f
nicolocamassa <camassanicolo03@gmail.com>
2023-11-24 15:11:19 +0100

    [TUTTI-FRUTTI FINITO] added comments

--- array-lista/js/script.js
+++ array-lista/js/script.js
@@ -1,13 +1,16 @@
+/* Inizializzazione delle variabili */
 let fruits = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'];
 let table = 'pesca';
 let flag = false;
 
+/* Controllo sul vettore con confronto sulla stringa */
 for(let i = 0; i < fruits.length; i++){
     if(fruits[i] == 'cocomero'){
         flag = true
     }
 }
 
+/* Se la flag è true o false manda in output i due console.log */
 if(flag){
     console.log("Trovato! Devo solo preparare il cocktail.")
 }else{
