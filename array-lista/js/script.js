b69d6a48b0ef8effcf162fd83aaf64c8061053bf
nicolocamassa <camassanicolo03@gmail.com>
2023-11-24 15:09:37 +0100

    [UPDATE] output success or failure added

--- array-lista/js/script.js
+++ array-lista/js/script.js
@@ -8,5 +8,9 @@ for(let i = 0; i < fruits.length; i++){
     }
 }
 
-
+if(flag){
+    console.log("Trovato! Devo solo preparare il cocktail.")
+}else{
+    console.log("Oh no, devo uscire a comprare il cocomero!")
+}
 
